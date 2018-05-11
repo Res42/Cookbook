fbb1b5c1b3399e229dc3a4b94e87d11c9027ce84
Adam Reisinger <reisinger.adam@gmail.com>
2018-05-11 11:53:07 +0200

    Fix analytics.

--- app/src/main/java/hu/bme/r0uj46/cookbook/CookbookApplication.java
+++ app/src/main/java/hu/bme/r0uj46/cookbook/CookbookApplication.java
@@ -1,8 +1,6 @@
 package hu.bme.r0uj46.cookbook;
 
 import com.crashlytics.android.Crashlytics;
-import com.google.android.gms.analytics.GoogleAnalytics;
-import com.google.android.gms.analytics.Tracker;
 import com.orm.SugarApp;
 
 import hu.bme.r0uj46.cookbook.ui.UIModule;
@@ -10,9 +8,6 @@ import io.fabric.sdk.android.Fabric;
 
 public class CookbookApplication extends SugarApp {
     public static CookbookApplicationComponent injector;
-
-    private static GoogleAnalytics sAnalytics;
-    private static Tracker sTracker;
 
     @Override
     public void onCreate() {
@@ -22,20 +17,6 @@ public class CookbookApplication extends SugarApp {
                 .uIModule(new UIModule(this))
                 .build();
 
-        sAnalytics = GoogleAnalytics.getInstance(this);
         Fabric.with(this, new Crashlytics());
-    }
-
-    /**
-     * Gets the default {@link Tracker} for this Application.
-     * @return tracker
-     */
-    synchronized public Tracker getDefaultTracker() {
-        // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
-        if (sTracker == null) {
-            sTracker = sAnalytics.newTracker(R.xml.global_tracker);
-        }
-
-        return sTracker;
     }
 }
--- app/src/main/java/hu/bme/r0uj46/cookbook/ui/AnalyticsActivity.java
+++ app/src/main/java/hu/bme/r0uj46/cookbook/ui/AnalyticsActivity.java
@@ -4,28 +4,16 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
-import com.google.android.gms.analytics.HitBuilders;
-import com.google.android.gms.analytics.Tracker;
-
-import hu.bme.r0uj46.cookbook.CookbookApplication;
+import com.google.firebase.analytics.FirebaseAnalytics;
 
 public abstract class AnalyticsActivity extends AppCompatActivity {
-    protected Tracker tracker;
+    protected FirebaseAnalytics firebaseAnalytics;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Obtain the shared Tracker instance.
-        CookbookApplication application = (CookbookApplication) getApplication();
-        tracker = application.getDefaultTracker();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        tracker.setScreenName(this.getClass().getSimpleName());
-        tracker.send(new HitBuilders.ScreenViewBuilder().build());
+        // Obtain the FirebaseAnalytics instance.
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this);
     }
 }
